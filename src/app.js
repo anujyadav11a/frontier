4a71b878b44d335ec43a02b75ec945bd1d397b33
Anuj Yadav <anujyadav11a@gmail.com>
2026-02-20 00:51:30 +0530

    configure google auth route

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser'; 
-const app = express();
+import googleOAuthRoutes from './routes/googleOAuth.routes.js';
+import { sessionMiddleware } from './middleware/session.middleware.js';
+import { testOAuth } from './controllers/test.controller.js';
 
+const app = express();
 
 const Options={
     origin:process.env.CORS_ORIGIN,
     Credential:true
 }
 
-
-
 app.use(cors(Options))
+app.use(express.json({limit:"10kb"}))
+app.use(express.urlencoded({limit:"10kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+app.use(sessionMiddleware)
 
- app.use(express.json({limit:"10kb"}))
- app.use(express.urlencoded({limit:"10kb"}))
- app.use(express.static("public"))
- app.use(cookieParser())
- 
+// Test route
+app.get('/test-oauth', testOAuth);
 
+// OAuth routes
+app.use('/auth', googleOAuthRoutes);
 
- export default app
+export default app
